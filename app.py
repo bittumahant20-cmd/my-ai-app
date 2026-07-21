28c46bfb80f1554ecfe8426f83220b928fbf2a19
bittumahant20-cmd <bittumahant20@gmail.com>
2026-07-22 02:35:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if st.button("AI se Poochein"):
         try:
             client = genai.Client(api_key=api_key)
             response = client.models.generate_content(
-                model='gemini-2.5-flash',
+                model='gemini-1.5-flash',
                 contents=user_question
             )
             st.success("Jawab:")
